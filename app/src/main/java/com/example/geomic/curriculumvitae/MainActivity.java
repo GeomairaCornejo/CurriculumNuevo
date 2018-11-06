4ba26636440052954ca682b1a7492183b6f91c84
Geomaira Cornejo <geomaira.cornejom@ug.edu.ec>
2018-11-06 16:10:17 -0500

    Ingreso de datos de la referencia

--- app/src/main/java/com/example/geomic/curriculumvitae/MainActivity.java
+++ app/src/main/java/com/example/geomic/curriculumvitae/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Se ha guardado exitosamente!!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
